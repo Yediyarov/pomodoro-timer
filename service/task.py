a8ef2a08a24bf4879cbe93cb0a3aa7832384ed96
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-22 19:00:21 +0400

    feat: implement task creation method in TaskService

--- service/task.py
+++ service/task.py
@@ -1,8 +1,7 @@
-
 from dataclasses import dataclass
 from repository.task import TaskRepository
 from repository.cache_task import TaskCache
-from schema.task import TaskSchema
+from schema.task import TaskSchema, TaskCreateSchema
 
 
 @dataclass
@@ -26,5 +25,16 @@ class TaskService:
             tasks = self.task_repository.get_task_by_category(category_name)
             self.task_cache.set_tasks(tasks)
             return tasks
+        
+    def create_task(self, task: TaskCreateSchema) -> TaskSchema:
+        # Create task in database
+        created_task = self.task_repository.create_task(task)
+        
+        # Update cache with new task list
+        tasks = self.task_repository.get_all_tasks()
+        tasks_schema = [TaskSchema.model_validate(task) for task in tasks]
+        self.task_cache.set_tasks(tasks_schema)
+        
+        return created_task
         
         
