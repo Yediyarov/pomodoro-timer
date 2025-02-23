bdebd6a5ef4256e327c77e4ecf5ef9b1b9c5f82b
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-23 13:44:15 +0400

    feat: add CRUD endpoints for tasks in router

--- handlers/tasks.py
+++ handlers/tasks.py
@@ -1,11 +1,38 @@
 from fastapi import APIRouter, Depends
-from schema import TaskSchema
+from schema import TaskSchema, TaskCreateSchema
 from dependency import get_task_service
 from service import TaskService
-
+from typing import Annotated
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 @router.get("/all", response_model=list[TaskSchema])
-async def get_tasks(task_service: TaskService = Depends(get_task_service)):
+async def get_tasks(
+    task_service: Annotated[TaskService, Depends(get_task_service)]
+    ):
     return task_service.get_all_tasks()
+
+
+@router.post("/", response_model=TaskSchema)
+async def create_task(
+    task: TaskCreateSchema,
+    task_service: Annotated[TaskService, Depends(get_task_service)]
+):
+    return task_service.create_task(task)
+
+
+@router.put("/{task_id}", response_model=TaskSchema)
+async def update_task(
+    task_id: int,
+    task: TaskSchema,
+    task_service: Annotated[TaskService, Depends(get_task_service)]
+):
+    return task_service.update_task(task_id, task)
+
+
+@router.delete("/{task_id}", response_model=TaskSchema)
+async def delete_task(
+    task_id: int,
+    task_service: Annotated[TaskService, Depends(get_task_service)]
+):
+    return task_service.delete_task(task_id)
