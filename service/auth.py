4ff38e1ffe923fa124c068671611658fadab2e6d
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-24 19:13:25 +0400

    refactor: separate access token generation logic and remove from user repository

--- service/auth.py
+++ service/auth.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from repository import UserRepository
 from exeptions import UserNotFound, InvalidCredentials
 from repository.user import UserProfile
-
+import uuid
 @dataclass
 class AuthService:
     user_repository: UserRepository
@@ -11,7 +11,8 @@ class AuthService:
     def login(self, username: str, password: str) -> UserLoginSchema:
         user = self.user_repository.get_user_by_username(username)
         self._validate_auth_user(user, password)
-        return UserLoginSchema(id=user.id, access_token=user.access_token)
+        access_token = self.generate_access_token(user_id=user.id)
+        return UserLoginSchema(id=user.id, access_token=access_token)
 
     @staticmethod
     def _validate_auth_user(user: UserProfile, password: str):
@@ -19,3 +20,7 @@ class AuthService:
             raise UserNotFound
         if user.password != password:
             raise InvalidCredentials
+        
+    @staticmethod
+    def generate_access_token(user_id: int) -> str:
+        return str(uuid.uuid4())
--- service/user.py
+++ service/user.py
@@ -1,19 +1,18 @@
 from dataclasses import dataclass
 from repository import UserRepository
 from schema import UserLoginSchema
-import uuid
+from service import AuthService
 
 @dataclass
 class UserService:
     user_repository: UserRepository
-    
+    auth_service: AuthService
+
     def create_user(self, username, password) -> UserLoginSchema:
-        access_token = self._generate_access_token()
-        user = self.user_repository.create_user(username, password, access_token)
-        return UserLoginSchema(id=user.id, access_token=user.access_token)
+        user = self.user_repository.create_user(username, password)
+        access_token = self.auth_service.generate_access_token(user.id)
+        return UserLoginSchema(id=user.id, access_token=access_token)
 
-    def _generate_access_token(self) -> str:
-        return str(uuid.uuid4())
-
+    
 
 
