0bdafa3d76d67d5d68f3c9ecf69ffa57eaddc5de
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-24 18:35:14 +0400

    refactor: make access_token nullable in UserProfile model

--- models/users.py
+++ models/users.py
@@ -1,6 +1,6 @@
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-
+from typing import Optional
 
 class UserProfile(Base):
     __tablename__ = "users"
@@ -8,4 +8,4 @@ class UserProfile(Base):
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     username: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    access_token: Mapped[str] = mapped_column(nullable=False)
+    access_token: Mapped[Optional[str]]
