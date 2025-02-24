f3e6cbc2cab29e46ac3d3861276aa27f2811887c
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-24 19:13:35 +0400

    refactor: remove access_token column from UserProfile model

--- models/users.py
+++ models/users.py
@@ -1,6 +1,5 @@
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from typing import Optional
 
 class UserProfile(Base):
     __tablename__ = "users"
@@ -8,4 +7,3 @@ class UserProfile(Base):
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     username: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    access_token: Mapped[Optional[str]]
