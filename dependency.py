7b9babfd9c5800b3ffeb0f28c1e9b3aa27f3a850
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-24 21:35:33 +0400

    feat: add dependency injection for authentication and user request context

--- dependency.py
+++ dependency.py
@@ -2,8 +2,12 @@ from database import get_db_session
 from cache import get_redis_connection
 from repository import TaskRepository, TaskCache, UserRepository
 from service import TaskService, UserService, AuthService
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
+from settings import Settings
+from fastapi import security
+from exeptions import InvalidToken, TokenExpired
+
 
 def get_tasks_repository(db_session: Session = Depends(get_db_session)) -> TaskRepository:
     return TaskRepository(db_session=db_session)
@@ -21,12 +25,32 @@ def get_task_service(
 def get_user_repository(db_session: Session = Depends(get_db_session)) -> UserRepository:
     return UserRepository(db_session=db_session)
 
-def get_user_service(
-        user_repository: UserRepository = Depends(get_user_repository)
-) -> UserService:
-    return UserService(user_repository)
+def get_settings() -> Settings:
+    return Settings()
 
 def get_auth_service(
-        user_repository: UserRepository = Depends(get_user_repository)
+        user_repository: UserRepository = Depends(get_user_repository),
+        settings: Settings = Depends(get_settings)
 ) -> AuthService:
-    return AuthService(user_repository)
+    return AuthService(user_repository, settings)
+
+def get_user_service(
+        user_repository: UserRepository = Depends(get_user_repository),
+        auth_service: AuthService = Depends(get_auth_service)
+) -> UserService:
+    return UserService(user_repository, auth_service)
+
+
+reuseable_oauth2 = security.HTTPBearer()
+
+def get_request_user_id(
+        auth_service: AuthService = Depends(get_auth_service), 
+        token: str = Depends(reuseable_oauth2)
+        ) -> int:
+    try:
+        user_id = auth_service.get_user_id_from_token(token.credentials)
+    except InvalidToken as e:
+        raise HTTPException(status_code=401, detail=e.detail)
+    except TokenExpired as e:
+        raise HTTPException(status_code=401, detail=e.detail)
+    return user_id
