b9c256d80ffaae8e7a445140f2567458b3bf774d
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-24 18:23:23 +0400

    feat: add dependency injection for user-related services and repositories

--- dependency.py
+++ dependency.py
@@ -1,12 +1,12 @@
 from database import get_db_session
 from cache import get_redis_connection
-from repository import TaskRepository, TaskCache
-from service import TaskService
+from repository import TaskRepository, TaskCache, UserRepository
+from service import TaskService, UserService, AuthService
 from fastapi import Depends
+from sqlalchemy.orm import Session
 
-def get_tasks_repository() -> TaskRepository:
-    db_session = get_db_session()
-    return TaskRepository(db_session)
+def get_tasks_repository(db_session: Session = Depends(get_db_session)) -> TaskRepository:
+    return TaskRepository(db_session=db_session)
 
 def get_tasks_cache() -> TaskCache:
     redis_connection = get_redis_connection()
@@ -17,3 +17,16 @@ def get_task_service(
         task_cache: TaskCache = Depends(get_tasks_cache)
         ) -> TaskService:
     return TaskService(task_repository, task_cache)
+
+def get_user_repository(db_session: Session = Depends(get_db_session)) -> UserRepository:
+    return UserRepository(db_session=db_session)
+
+def get_user_service(
+        user_repository: UserRepository = Depends(get_user_repository)
+) -> UserService:
+    return UserService(user_repository)
+
+def get_auth_service(
+        user_repository: UserRepository = Depends(get_user_repository)
+) -> AuthService:
+    return AuthService(user_repository)
