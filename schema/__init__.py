0cbbfdf7c44492a412b605c2a0f84f3e1410f4b2
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-24 18:23:04 +0400

    feat: add UserLoginSchema and UserRegisterSchema to schema module

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,3 +1,4 @@
 from schema.task import TaskSchema, TaskCreateSchema, CategorySchema
+from schema.user import UserLoginSchema, UserRegisterSchema
 
-__all__ = ["TaskSchema", "TaskCreateSchema", "CategorySchema"]
+__all__ = ["TaskSchema", "TaskCreateSchema", "CategorySchema", "UserLoginSchema", "UserRegisterSchema"]
