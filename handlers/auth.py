8485674860a62aa7106675c5839bd119746d6fa6
Yediyarov <yediyarov.xeyal@gmail.com>
2025-02-26 00:31:08 +0400

    feat: add Google OAuth login routes to auth handler

--- handlers/auth.py
+++ handlers/auth.py
@@ -6,6 +6,7 @@ from typing import Annotated
 from fastapi import Depends
 from exeptions import UserNotFound, InvalidCredentials
 from fastapi import HTTPException
+from fastapi.responses import RedirectResponse
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -17,3 +18,13 @@ def login(body: UserRegisterSchema, auth_service: Annotated[AuthService, Depends
         raise HTTPException(status_code=401, detail="Invalid credentials")
     except InvalidCredentials:
         raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+
+@router.get("/login/google", response_class=RedirectResponse)
+async def google_login(auth_service: Annotated[AuthService, Depends(get_auth_service)]):
+    redirect_url = auth_service.get_google_redirect_url()
+    return RedirectResponse(url=redirect_url)
+
+@router.get("/google")
+async def google_auth(auth_service: Annotated[AuthService, Depends(get_auth_service)], code: str):
+    return auth_service.google_auth(code)
